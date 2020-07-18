0f6bed39a85b7b30df65790dec3af30669e51487
Wen Xu <wxu63@my.centennialcollege.ca>
2020-07-17 21:21:50 -0400

    fix object giantplanet  spelling mistake

--- Assignment3AbstractPlanets/Program.cs
+++ Assignment3AbstractPlanets/Program.cs
@@ -6,32 +6,29 @@ namespace Assignment3AbstractPlanets
     {
         //--Author's name:Wen Xu
         //--Author’s student number:301098127,
-        //--Date last Modified:11,July,2020,
+        //--Date last Modified:12,July,2020,
         //--Program description:1.0.0 version is to create four classes(Planet,GiantPlanet,TerrestrialPlanet,Program)
         //and three interfaces(IHasMoons,IHasRings,Ihabitable).The output should display giantPlanet's attributes
         //(name,diameter and mass) as well as terrestrialPlanet's attributes(name, diameter and mass)
         //--Revision History:1.0.0
+        // Implement method WaitForAnyKey
+       
 
         static void Main(string[] args)
         {   // implement GiantPlanet class
-            GiantPlanet giantPlant=new GiantPlanet("Jupiter", 139820,1.898E27, "ice");
-            Console.WriteLine(giantPlant.ToString());
+            GiantPlanet giantPlanet=new GiantPlanet("Jupiter", 139820,1.898E27, "ice");
+            Console.WriteLine(giantPlanet.ToString());
            
 
             // Implement TerrestrialPlant class
             TerrestrialPlanet terrestrialPlanet=new TerrestrialPlanet("Mars", 6779, 6.39E23, false);
             Console.WriteLine(terrestrialPlanet.ToString());
-            
 
-            // Implement method WaitForAnyKey
+
             static void WaitForAnyKey()
             {
                 Console.ReadKey();
             }
-
-
-
-
 
         }
     }
